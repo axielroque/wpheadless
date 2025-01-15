db4bf1dc06536d3d32df250b09718cf8dca949b7
Axiel Roque <axiel.roque@outlook.es>
2025-01-15 12:31:48 -0500

    setting: frontend

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Navbar from "$lib/modules/navbar/navbar.svelte";
+</script>
+
+<Navbar />
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
