7e968e9922f29ba5bf6f9a1f8f269aea4afd49e7
Axiel Roque <axiel.roque@outlook.es>
2025-01-16 10:58:17 -0500

    homepage

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,26 @@
 <script>
   import Navbar from "$lib/modules/navbar/navbar.svelte";
+  import Footer from "$lib/modules/footer/footer.svelte";
+  import Hero from "$lib/components/banners/hero.svelte";
+  import Stat from "$lib/components/stat/stat.svelte";
+  import ProductCollection from "$lib/components/product-collection/product-collection.svelte";
+  import BlogCards from "$lib/components/blog-cards/blog-cards.svelte";
+  import Newsletter from "$lib/components/ctas/newsletter.svelte";
 </script>
 
 <Navbar />
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<Hero />
+
+<ProductCollection />
+
+<Stat />
+
+<BlogCards />
+
+<Newsletter />
+
+<Footer />
 
 <style lang="postcss">
   :global(html) {
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: "#2ba9bc",
+        accent: "#404040"
+      }
+    }
   },
   plugins: []
 };
