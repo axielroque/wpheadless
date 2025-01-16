406a622e634a1647187d83961d4426f6586abf89
Axiel Roque <32523259+axielroque@users.noreply.github.com>
2025-01-16 11:27:30 -0500

    Dev (#1)
    * setting: frontend
    
    * Add: components
    
    * Add: module/footer
    
    * homepage

---
+++ frontend/src/lib/modules/navbar/navbar.svelte
@@ -0,0 +1,105 @@
+<header class="bg-white">
+  <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+    <div class="flex h-16 items-center justify-between">
+      <div class="md:flex md:items-center md:gap-12">
+        <a class="flex items-center text-primary" href="#">
+          <span class="block w-14">
+            <svg
+              fill="#404040"
+              width="auto"
+              height="auto"
+              viewBox="0 0 24 24"
+              id="moon"
+              data-name="Line Color"
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon line-color"
+              ><circle
+                id="secondary"
+                cx="9"
+                cy="12"
+                r="2"
+                style="fill: none; stroke: rgb(44, 169, 188); stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"
+              ></circle><line
+                id="secondary-upstroke"
+                x1="16.45"
+                y1="13.5"
+                x2="16.55"
+                y2="13.5"
+                style="fill: none; stroke: rgb(44, 169, 188); stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"
+              ></line><circle
+                id="primary"
+                cx="12"
+                cy="12"
+                r="9"
+                style="fill: none; stroke: #404040; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"
+              ></circle></svg
+            >
+          </span>
+          <span class="text-xl">TechStore</span>
+        </a>
+      </div>
+
+      <div class="hidden md:block">
+        <nav aria-label="Global">
+          <ul class="flex items-center gap-6 text-md font-medium">
+            <li>
+              <a class="text-accent transition hover:text-gray-500/75" href="#">
+                Shop
+              </a>
+            </li>
+
+            <li>
+              <a
+                class="text-accent transition hover:text-gray-500/75"
+                href="/blog"
+              >
+                Blog
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div class="flex items-center gap-4">
+        <div class="sm:flex sm:gap-4">
+          <a
+            class="rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white shadow"
+            href="#"
+          >
+            Login
+          </a>
+
+          <div class="hidden sm:flex">
+            <a
+              class="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-primary"
+              href="#"
+            >
+              Register
+            </a>
+          </div>
+        </div>
+
+        <div class="block md:hidden">
+          <button
+            class="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="size-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Navbar from "$lib/modules/navbar/navbar.svelte";
+  import Footer from "$lib/modules/footer/footer.svelte";
+  import Hero from "$lib/components/banners/hero.svelte";
+  import Stat from "$lib/components/stat/stat.svelte";
+  import ProductCollection from "$lib/components/product-collection/product-collection.svelte";
+  import BlogCards from "$lib/components/blog-cards/blog-cards.svelte";
+  import Newsletter from "$lib/components/ctas/newsletter.svelte";
+</script>
+
+<Navbar />
+
+<Hero />
+
+<ProductCollection />
+
+<Stat />
+
+<BlogCards />
+
+<Newsletter />
+
+<Footer />
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
